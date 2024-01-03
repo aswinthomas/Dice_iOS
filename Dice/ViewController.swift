15aa0aedfe0c1729b604699b9a4c4a1aa4afe5c1
aswinthomas <aswinthomas@gmail.com>
2024-01-03 18:09:16 +0800

    add ss

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -12,12 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
         print("Button got pressed")
         

15d1e0e7f7a58cf5cc35608c101510b42dca5606
aswinthomas <aswinthomas@gmail.com>
2024-01-03 16:36:28 +0800

    add icon

--- Dice/ViewController.swift
+++ Dice/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        print("Button got pressed")
+        
+        let images: [UIImage?] = [UIImage(named: "DiceOne"),
+                                  UIImage(named: "DiceTwo"),
+                                  UIImage(named: "DiceThree"),
+                                  UIImage(named: "DiceFour"),
+                                  UIImage(named: "DiceFive"),
+                                  UIImage(named: "DiceSix")]
+        let leftDiceNumber = Int.random(in: 0..<images.count)
+        let rightDiceNumber = Int.random(in: 0..<images.count)
+        diceImageView1.image = images[leftDiceNumber]
+        diceImageView2.image = images[rightDiceNumber]
+    }
 }
 
